bbda238c3fba27c67de69e5360497ea5be63be0e
Amir Abdou <amir@amirsaad.com>
2012-12-03 21:59:33 -0800

    load sprite

---
+++ game.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+    var canvas = document.getElementById("game");
+    var ctx = canvas.getContext && canvas.getContext("2d");
+
+    if (!ctx) {
+        alert("Upgrade your browser.");
+    }
+    else {
+        startGame();
+    }
+
+    function startGame() {
+        SpriteSheet.load({
+            ship: { sx: 0, sy: 0, w: 18, h: 35, frames: 3 }
+        },function() {
+            SpriteSheet.draw(ctx,"ship",0,0);
+            SpriteSheet.draw(ctx,"ship",100,50);
+            SpriteSheet.draw(ctx,"ship",150,100,1);
+        });
+    }
+}
