6e70598399ec8342a1e1d74404248e2ba6925ea0
Amir Abdou <amir@amirsaad.com>
2012-12-04 21:42:32 -0800

    game object

--- engine.js
+++ engine.js
@@ -1,10 +1,90 @@
-var SpriteSheet = function() {
+// Game class
+var Game = new function() {
+    // Key codes
+    var keyCodes = {
+        37: 'left',
+        38: 'right',
+        39: 'fire'
+    };
+    
+    // Game boards
+    var boards = [
+    
+    ];
+    
+    // State of the keys
+    this.keys = {};
+    
+    // Init the game object
+    this.initialize = function(canvasId, spriteData, callback) {
+        this.canvas = document.getElementById(canvasId);
+        this.width  = this.canvas.width;
+        this.height = this.canvas.height;
+        this.ctx    = this.canvas.getContext && this.canvas.getContext('2d');
+        
+        if (!this.ctx) {
+            return alert("Your browser does not support canvas");
+        }
+        
+        // Set up input keys
+        this.setupInput();
+        
+        // Game loop
+        this.loop();
+        
+        // Load game sprite
+        SpriteSheet.load(spriteData, callback);
+    };
+    
+    // Set up input keys
+    this.setupInput = function() {
+        // Keydown turns the stats on
+        window.addEventListener('keydown', function(e) {
+            if (keyCodes[e.keyCode]) {
+                Game.keys[keyCodes[e.keyCode]] = true;
+                e.preventDefault();
+            }
+        });
+        
+        // Keyup turns the stats off
+        window.addEventListener('keyup', function(e) {
+            if (keyCodes[e.keyCode]) {
+                Game.keys[keyCodes[e.keyCode]] = false;
+                e.preventDefault();
+            }
+        });
+    };
+    
+    // Start the game loop
+    this.loop = function() {
+        var dt = 10 / 1000; // ??        
+        for (i=0; i < boards.length; i++) {
+            if (boards[i]) {
+                boards[i].step(dt);
+                
+                // The step may remove the board
+                boards[i] && boards[i].draw(Game.ctx);
+            }
+        }
+        
+        setTimeout(Game.loop, 30);
+    };
+    
+    // Set an active board
+    this.setBoard = function(board, num) {
+        boards[num] = board;
+    }
+};
+
+
+// Sprite sheet class
+var SpriteSheet = new function() {
     this.map = {};
     this.load = function(spriteData, callback) {
         this.map = spriteData;
         this.image = new Image();
         this.image.onload = callback;
-        this.src = "images/sprites.png";
+        this.image.src = "images/sprites.png";
     }
     
     this.draw = function(ctx,sprite,x,y,frame) {
