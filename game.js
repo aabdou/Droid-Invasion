9ecf16b397e69b2959e20fdbfe19125a87f37da0
Amir Abdou <amir@amirsaad.com>
2012-12-04 21:51:20 -0800

    refactor to use the game object

--- game.js
+++ game.js
@@ -1,22 +1,11 @@
-window.onload = function() {
+var sprites = {
+    ship: { sx: 0, sy: 0, w: 18, h: 35, frames: 3 }
+};    
 
-    var canvas = document.getElementById("game");
-    var ctx = canvas.getContext && canvas.getContext("2d");
-
-    if (!ctx) {
-        alert("Upgrade your browser.");
-    }
-    else {
-        startGame();
-    }
-
-    function startGame() {
-        SpriteSheet.load({
-            ship: { sx: 0, sy: 0, w: 18, h: 35, frames: 3 }
-        },function() {
-            SpriteSheet.draw(ctx,"ship",0,0);
-            SpriteSheet.draw(ctx,"ship",100,50);
-            SpriteSheet.draw(ctx,"ship",150,100,1);
-        });
-    }
+var startGame = function () {
+    SpriteSheet.draw(Game.ctx,"ship",100,100,1);        
 }
+
+window.addEventListener('load', function() {
+    Game.initialize("game", sprites, startGame);
+});
